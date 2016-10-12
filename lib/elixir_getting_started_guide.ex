d5937745add4b3de4e51fd5e320bfe1be21d54f2
Milton Mazzarri <milmazz@gmail.com>
2016-10-12 08:01:15 -0500

    Map links for each guide

--- lib/elixir_getting_started_guide.ex
+++ lib/elixir_getting_started_guide.ex
@@ -20,15 +20,16 @@ defmodule ElixirGettingStartedGuide do
       |> YamlElixir.read_from_file()
       |> generate_nav(options)
 
-    nav |> convert_markdown_pages() |> to_epub(nav, options)
+    nav |> convert_markdown_pages(options) |> to_epub(nav, options)
   end
 
   defp generate_nav(yaml, options) do
     yaml =
       case options[:guide] do
+        :getting_started -> List.first(yaml)
         :mix_otp -> Enum.at(yaml, 1)
         :meta -> List.last(yaml)
-        _ -> List.first(yaml)
+        _ -> raise "invalid guide, allowed: :mix_otp, :meta or :getting_started"
       end
 
     Enum.flat_map(List.wrap(yaml), fn(section) ->
@@ -39,20 +40,20 @@ defmodule ElixirGettingStartedGuide do
     end)
   end
 
-  defp convert_markdown_pages(config) do
+  defp convert_markdown_pages(config, options) do
     config
     |> Enum.map(&Task.async(fn ->
-        to_xhtml(&1)
+        to_xhtml(&1, options)
        end))
     |> Enum.map(&Task.await(&1, :infinity))
   end
 
-  defp to_xhtml(%{content: path, dir: dir} = nav) do
+  defp to_xhtml(%{content: path, dir: dir} = nav, options) do
     content =
       "#{@submodule}#{dir}#{path}"
       |> String.replace(~r/(.*)\.xhtml/, "\\1.markdown")
       |> File.read!()
-      |> clean_markdown()
+      |> clean_markdown(options)
       |> Markdown.to_html(autolink: true, fenced_code: true, tables: true)
       |> wrap_html(nav)
 
@@ -70,7 +71,8 @@ defmodule ElixirGettingStartedGuide do
       case options[:guide] do
         :mix_otp -> "Mix and OTP"
         :meta -> "Meta-programming in Elixir"
-        _ -> "Elixir Getting Started Guide"
+        :getting_started -> "Elixir Getting Started Guide"
+        _ -> raise "invalid guide, allowed: :mix_otp, :meta or :getting_started"
       end
 
     config = %BUPE.Config{
@@ -91,12 +93,12 @@ defmodule ElixirGettingStartedGuide do
     title |> String.replace(" ", "-") |> String.downcase()
   end
 
-  defp clean_markdown(content) do
+  defp clean_markdown(content, options) do
     content
     |> String.replace("{% include toc.html %}", "")
     |> String.replace(~r/# {{ page.title }}(<span hidden>.<\/span>)?/, "") # The <span hidden>.</span> is a hack used in pattern-matching.md
     |> remove_frontmatter()
-    |> map_links()
+    |> map_links(options)
   end
 
   defp remove_frontmatter(content) do
@@ -104,15 +106,15 @@ defmodule ElixirGettingStartedGuide do
     content
   end
 
-  defp map_links(content) do
+  defp map_links(content, options) do
     Regex.replace(~r/\[([^\]]+)\]\(([^\)]+)\)/, content, fn(_, text, href) ->
       case URI.parse(href) do
         %URI{scheme: nil, path: "/getting-started/meta/" <> path } ->
-          "[#{text}](#{@homepage}/getting-started/meta/#{path})"
+          map_meta_links(text, path, options[:guide])
         %URI{scheme: nil, path: "/getting-started/mix-otp/" <> path } ->
-          "[#{text}](#{@homepage}/getting-started/mix-otp/#{path})"
+          map_mix_otp_link(text, path, options[:guide])
         %URI{scheme: nil, path: "/getting-started/" <> path } ->
-          "[#{text}](#{String.replace(path, "html", "xhtml")})"
+          map_getting_started_links(text, path, options[:guide])
         %URI{scheme: nil, path: "/" <> path } ->
           "[#{text}](#{@homepage}/#{path})"
         _ ->
@@ -120,6 +122,17 @@ defmodule ElixirGettingStartedGuide do
       end
     end)
   end
+
+  defp map_meta_links(text, path, :meta), do: map_section_links(text, path)
+  defp map_meta_links(text, path, _), do: "[#{text}](#{@homepage}/getting-started/meta/#{path})"
+
+  defp map_mix_otp_link(text, path, :mix_otp), do: map_section_links(text, path)
+  defp map_mix_otp_link(text, path, _), do: "[#{text}](#{@homepage}/getting-started/mix-otp/#{path})"
+
+  defp map_getting_started_links(text, path, :getting_started), do: map_section_links(text, path)
+  defp map_getting_started_links(text, path, _), do: "[#{text}](#{@homepage}/getting-started/#{path})"
+
+  defp map_section_links(text, path), do: "[#{text}](#{String.replace(path, "html", "xhtml")})"
 
   EEx.function_from_file(:defp, :wrap_html,
                          Path.expand("elixir_getting_started_guide/templates/page.eex", __DIR__),
